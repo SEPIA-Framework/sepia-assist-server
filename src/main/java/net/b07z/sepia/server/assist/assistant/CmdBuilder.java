b75d46d8a1153c5e4a18d7adb38818db5719dbd1
Florian Quirin <info@bytemind.de>
2020-05-18 18:11:44 +0200

    Update CmdBuilder.java

--- src/main/java/net/b07z/sepia/server/assist/assistant/CmdBuilder.java
+++ src/main/java/net/b07z/sepia/server/assist/assistant/CmdBuilder.java
@@ -15,7 +15,10 @@ public class CmdBuilder {
 	//-------- pre-build command summaries ---------
 
 	public static String getWayHomeInfo(String travelType, String language){
-		String cmdSum = CMD.DIRECTIONS + ";;location_start=<user_location>;;location_end=<user_home>;;travel_request_info=<duration>;;";
+		String cmdSum = CMD.DIRECTIONS + ";;" 
+				+ PARAMETERS.LOCATION_START + "=<user_location>;;"
+				+ PARAMETERS.LOCATION_END + "=<user_home>;;"
+				+ PARAMETERS.TRAVEL_REQUEST_INFO + "=<duration>;;";
 		if (!travelType.isEmpty()){
 			cmdSum += PARAMETERS.TRAVEL_TYPE + "={\"value\":\"" + travelType + "\",\"value_local\":\"" + TravelType.getLocal("<" + travelType + ">", language) + "\"};;";
 		}
@@ -23,7 +26,10 @@ public class CmdBuilder {
 	}
 
 	public static String getWayToWorkInfo(String travelType, String language){
-		String cmdSum = CMD.DIRECTIONS + ";;location_start=<user_location>;;location_end=<user_work>;;travel_request_info=<duration>;;";
+		String cmdSum = CMD.DIRECTIONS + ";;" 
+				+ PARAMETERS.LOCATION_START + "=<user_location>;;"
+				+ PARAMETERS.LOCATION_END + "=<user_work>;;"
+				+ PARAMETERS.TRAVEL_REQUEST_INFO + "=<duration>;;";
 		if (!travelType.isEmpty()){
 			cmdSum += PARAMETERS.TRAVEL_TYPE + "={\"value\":\"" + travelType + "\",\"value_local\":\"" + TravelType.getLocal("<" + travelType + ">", language) + "\"};;";
 		}
@@ -31,34 +37,44 @@ public class CmdBuilder {
 	}
 
 	public static String getSoccerTable(String league){
-		String cmdSum = CMD.NEWS + ";;news_type={\"value\":\"table\"};;news_section={\"value\":\"soccer\"};;";
+		String cmdSum = CMD.NEWS + ";;" 
+				+ PARAMETERS.NEWS_TYPE + "={\"value\":\"table\"};;"
+				+ PARAMETERS.NEWS_SECTION + "={\"value\":\"soccer\"};;";
 		if (league != null && !league.isEmpty()){
-			cmdSum += "sports_league={\"value\":\"" + league + "\"};;";
+			cmdSum += PARAMETERS.SPORTS_LEAGUE + "={\"value\":\"" + league + "\"};;";
 		}
 		return cmdSum;
 	}
 
 	public static String getSoccerResults(String league, String team){
-		String cmdSum = CMD.NEWS + ";;news_type={\"value\":\"results\"};;news_section={\"value\":\"soccer\"};;";
+		String cmdSum = CMD.NEWS + ";;" 
+				+ PARAMETERS.NEWS_TYPE + "={\"value\":\"results\"};;"
+				+ PARAMETERS.NEWS_SECTION + "={\"value\":\"soccer\"};;";
 		if (team != null && !team.isEmpty()){
-			cmdSum += "sports_team={\"value\":\"" + team + "\"};;";
+			cmdSum += PARAMETERS.SPORTS_TEAM + "={\"value\":\"" + team + "\"};;";
 		}
 		if (league != null && !league.isEmpty()){
-			cmdSum += "sports_league={\"value\":\"" + league + "\"};;";
+			cmdSum += PARAMETERS.SPORTS_LEAGUE + "={\"value\":\"" + league + "\"};;";
 		}
 		return cmdSum;
 	}
 
 	public static String getFood(String foodType){
-		return CMD.FOOD + ";;food_class=;;food_item=;;";
+		return CMD.FOOD + ";;" 
+				+ PARAMETERS.FOOD_CLASS + "=;;" 
+				+ PARAMETERS.FOOD_ITEM + "=;;";
 	}
 
 	public static String getBrunchLocation(){
-		return CMD.LOCATION + ";;" + PARAMETERS.SEARCH + "=<user_location>;;poi=brunch;;";
+		return CMD.LOCATION + ";;" 
+				+ PARAMETERS.SEARCH + "=<user_location>;;" 
+				+ PARAMETERS.POI + "=brunch;;";
 	}
 	
 	public static String getRestaurantLocation(){
-		return CMD.LOCATION + ";;" + PARAMETERS.SEARCH + "=<user_location>;;poi=restaurant;;";
+		return CMD.LOCATION + ";;" 
+				+ PARAMETERS.SEARCH + "=<user_location>;;" 
+				+ PARAMETERS.POI + "=restaurant;;";
 	}
 
 	public static String getNews(String newsSection){
@@ -70,20 +86,24 @@ public class CmdBuilder {
 	}
 
 	public static String getWebSearch(String search){
-		return CMD.WEB_SEARCH + ";;" + PARAMETERS.WEBSEARCH_REQUEST + "=" + search;
+		return CMD.WEB_SEARCH + ";;" 
+				+ PARAMETERS.WEBSEARCH_REQUEST + "=" + search;
 	}
 
 	public static String getWiki(String search){
-		return CMD.KNOWLEDGEBASE + ";;" + PARAMETERS.SEARCH + "=" + search;
+		return CMD.KNOWLEDGEBASE + ";;" 
+				+ PARAMETERS.SEARCH + "=" + search;
 	}
 
 	public static String getRadioStation(String station){
 		//return CMD.MUSIC_RADIO + ";;" + PARAMETERS.RADIO_STATION + "={\"value\":\"" + station + "\"};;";
-		return CMD.MUSIC_RADIO + ";;" + PARAMETERS.RADIO_STATION + "=" + station + ";;";
+		return CMD.MUSIC_RADIO + ";;" 
+				+ PARAMETERS.RADIO_STATION + "=" + station + ";;";
 	}
 	public static String getRadioGenre(String genre){
 		//return CMD.MUSIC_RADIO + ";;" + PARAMETERS.RADIO_STATION + "={\"value\":\"" + station + "\"};;";
-		return CMD.MUSIC_RADIO + ";;" + PARAMETERS.MUSIC_GENRE + "=" + genre + ";;";
+		return CMD.MUSIC_RADIO + ";;" 
+				+ PARAMETERS.MUSIC_GENRE + "=" + genre + ";;";
 	}
 
 	public static String getTvProgram(String time){
