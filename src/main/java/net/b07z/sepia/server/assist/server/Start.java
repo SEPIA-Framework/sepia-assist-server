cb7ae4914bb67272be9f73b2aa13ecd4c65375d8
Florian Quirin <info@bytemind.de>
2020-07-05 20:52:53 +0200

    added new '/get-service-source' endpoint to load custom service source code

--- src/main/java/net/b07z/sepia/server/assist/server/Start.java
+++ src/main/java/net/b07z/sepia/server/assist/server/Start.java
@@ -333,6 +333,7 @@ public class Start {
 		get("/upload-service", (request, response) -> 		SdkEndpoint.uploadServiceGet(request, response));
 		post("/upload-service", (request, response) -> 		SdkEndpoint.uploadServicePost(request, response));
 		post("/get-services", (request, response) -> 		SdkEndpoint.getServicesPost(request, response));
+		post("/get-service-source", (request, response) -> 	SdkEndpoint.getServiceSourceCodePost(request, response));
 		post("/delete-service", (request, response) -> 		SdkEndpoint.deleteServicePost(request, response));
 		
 		//Remote controls
