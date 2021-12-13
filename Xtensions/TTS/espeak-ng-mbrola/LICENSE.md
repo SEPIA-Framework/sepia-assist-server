918f3bcda734df7706e2fbcf1f28860267357e4f
Florian Quirin <info@bytemind.de>
2021-12-13 03:49:16 +0100

    added MBROLA for espeak install instructions (wip)

---
+++ Xtensions/TTS/espeak-ng-mbrola/LICENSE.md
@@ -0,0 +1,11 @@
+# TTS Engine: MBROLA Voices for Espeak-NG
+
+Info: https://github.com/espeak-ng/espeak-ng/blob/master/docs/mbrola.md  
+  
+In this folder you will find the install script for MBROLA voices. The voices are used as plugins for Espeak-NG so you don't have to switch the engine. They are equally efficient and fast, but can sound considerably more natural.  
+  
+Please read the [LICENSE](LICENSE.md) file! While the voices are open and free not all of them can be used commercially without asking the creator for permission. 
+
+## Installation
+
+TBD
---
+++ Xtensions/TTS/espeak-ng-mbrola/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+echo "This script will download and install MBROLA for espeak-ng and download a few selected voices."
+echo "If you need more voices feel free to edit this script ;-)."
+echo "MBROLA and the included voices can be used freely for private, non-commercial SEPIA projects."
+echo ""
+echo "If you are planning to use MBROLA in a public or commercial project please read the following"
+echo "LICENSE files carefully:"
+echo ""
+echo "MBROLA AGPL 3.0 license: https://github.com/numediart/MBROLA/blob/master/LICENSE"
+echo "MBROLA voices terms of use: https://github.com/numediart/MBROLA-voices/blob/master/LICENSE.md"
+echo ""
+read -p "Enter 'agree' to continue: " agreeornot
+echo ""
+if [ -n "$agreeornot" ] && [ $agreeornot = "agree" ]; then
+	echo "Ty, let's go!"
+else
+	echo "Ok. See you :-)"
+	exit
+fi
+# Install MBROLA
+echo "Downloading and building MBROLA..."
+sudo apt-get update
+sudo apt-get install make gcc
+git clone https://github.com/numediart/MBROLA.git
+cd MBROLA
+make
+sudo cp Bin/mbrola /usr/bin/mbrola
+echo ""
+echo "Downloading voices..."
+mkdir -p "voices/de4"
+wget -O voices/de4/de4 "https://github.com/numediart/MBROLA-voices/blob/master/data/de4/de4?raw=true"
+mkdir -p "voices/en1"
+wget -O voices/en1/en1 "https://github.com/numediart/MBROLA-voices/blob/master/data/en1/en1?raw=true"
+echo "Copying voices to '/usr/share/mbrola/'..."
+sudo mkdir -p "/usr/share/mbrola/"
+sudo cp -r voices/* /usr/share/mbrola/
+cd ..
+#echo "Cleaning up ..."
+#rm -rf "MBROLA"
+echo "DONE."
+
+
